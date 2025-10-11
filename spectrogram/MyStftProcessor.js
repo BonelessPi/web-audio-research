2fc76a2d9097d65f3ea6fe1fc1f2aa498852bdaa
BonelessPi <84295882+BonelessPi@users.noreply.github.com>
2025-10-11 19:57:06 -0400

    Got it working (pass through)
    - Needed to remove embind code because embind required the js glue, which can't be imported by the Processor class or passed to it for copy reasons.
    - Standalone WASM requires some imports.
    Add firefox support: Firefox doesn't supply empty arrays in some edge case, which caused an error
    - Update makefile

--- spectrogram/MyStftProcessor.js
+++ spectrogram/MyStftProcessor.js
@@ -5,31 +5,46 @@ class MyStftProcessor extends AudioWorkletProcessor {
         super();
 
         const wasmModule = options.processorOptions.wasmModule;
-        console.log(wasmModule);
         this.ready = false;
 
-        WebAssembly.instantiate(wasmModule, { env: {} })
+        const importObject = {
+            env: {
+                memory: new WebAssembly.Memory({initial: 256}),
+                table: new WebAssembly.Table({ initial: 0, element: 'anyfunc' }),
+                __memory_base: 0,
+                __table_base: 0,
+                emscripten_notify_memory_growth: () => {console.log("WARNING WASM MEM GROWTH!!")},
+                abort: msg => { throw new Error(`WASM abort: ${msg}`) },
+            }
+        };
+
+        WebAssembly.instantiate(wasmModule, importObject)
         .then(instance => {
             this.instance = instance;
             this.exports = instance.exports;
             this.memory = instance.exports.memory;
-            this.internalNodePtr = instance.exports._stft_internal_create();
+            this.internalNodePtr = instance.exports.stft_internal_create();
             this.ready = true;
         });
     }
 
     process(inputList, outputList, parameters) {
+        if(!this.ready){
+            return true;
+        }
+
+        // This function runs until the audio context is suspended
         // TODO handle multiple channels and inputs??
-        const input = inputList[0][0];
-        const output = outputList[0][0];
+        const input = inputList[0][0] ?? new Float32Array(128);
+        const output = outputList[0][0] ?? new Float32Array(128);
 
         // TODO check that write happened
         // TODO write as helper methods here
-        new Float32Array(this.instance.HEAPF32.buffer,this.exports._stft_internal_next_input_quantum_ptr(this.internalNodePtr),128).set(input);
-        output.set(new Float32Array(this.instance.HEAPF32.buffer,this.exports._stft_internal_next_output_quantum_ptr(this.internalNodePtr),128));
+        new Float32Array(this.memory.buffer,this.exports.stft_internal_next_input_quantum_ptr(this.internalNodePtr),128).set(input);
+        output.set(new Float32Array(this.memory.buffer,this.exports.stft_internal_next_output_quantum_ptr(this.internalNodePtr),128));
 
         // TODO assert same size input and output and quantum len
-        this.internalNode.process();
+        this.exports.stft_internal_process(this.internalNodePtr);
 
         return true;
     }
